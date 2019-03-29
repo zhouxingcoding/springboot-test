772a46f15c2e10190f1a553d490e8473ee7b48e9
zhouxingcoding <393169250@qq.com>
2019-03-29 21:45:43 +0800

    init

---
+++ spring-boot-base/src/main/java/com/boot/springbootbase/SpringBootBaseApplication.java
@@ -0,0 +1,13 @@
+package com.boot.springbootbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBaseApplication.class, args);
+    }
+
+}
